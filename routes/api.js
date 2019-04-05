c4152f964911e16ca0ce814c4768489a4a377902
CT Cheng (Anna) <annacheng310@gmail.com>
2019-04-05 21:58:12 +0900

    All tests passing

--- routes/api.js
+++ routes/api.js
@@ -8,16 +8,123 @@
 
 'use strict';
 
-var expect = require('chai').expect;
-var MongoClient = require('mongodb');
+let expect = require('chai').expect;
+let MongoClient = require('mongodb');
+let request = require('request');
+let rp = require('request-promise');
+// let Promise = require('./promise');
 
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+const CONNECTION_STRING = process.env.DB;
+//MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get(function (req, res){
-      
-    });
-    
-};
+      const stock = req.query.stock;
+      const like  = (req.query.like == 'true')? 1:0;
+      const ip    = req.ip;
+      // console.log(stock,like,ip)
+      if (typeof stock == 'string') {
+      MongoClient.connect(CONNECTION_STRING, function(err, db) {
+
+        // console.log('Connected')
+        const stockData = {stock:stock,price:0,likes:0}
+        const url = 'https://api.iextrading.com/1.0/stock/'+stock+'/price'
+
+        new Promise((resolve,reject) => {
+          db.collection('stocks').find({stock:stock}).toArray((err,result)=>{
+            if (result.length > 0 ) {
+              stockData["likes"] = result[0]["likes"]
+              resolve({modify: result[0]['like_ips'].includes(ip) == false & like===1})
+            } else {
+              resolve({modify: like===1})
+            }
+          })
+        })
+        .then((result)=>{
+          // console.log(result)
+          if (result.modify){
+            console.log('Modify')
+            db.collection('stocks').findAndModify(
+                      {stock: stock},
+                      {},
+                      { $push: {like_ips: ip},
+                        $inc: {likes: like} },
+                      {new:true, upsert: true},
+                      (err,result)=>{
+                        if (err) throw err
+                        stockData["likes"] = result.value["likes"]
+                        // console.log(stockData)
+              })
+          }
+        }).then((result)=>{
+          rp(url).then(price => {
+            stockData["price"] = price
+            console.log(stockData)
+            res.json({stockData})
+          });
+        })
+        .catch((err) => { console.log(err) })
+      });
+    } else {
+      MongoClient.connect(CONNECTION_STRING, function(err, db) {
+        // console.log('Connected')
+        const stockData = [{stock:0,price:0,rel_likes:0},
+          {stock:0,price:0,rel_likes:0}]
+        const likes = [0,0]
+
+        let promisesApi = stock.map((s,i)=> {
+          const url = 'https://api.iextrading.com/1.0/stock/'+s+'/price'
+          stockData[i]['stock'] = s
+          return rp(url).then(price => {
+            stockData[i]["price"] = price
+          });
+        })
+
+        let promisesDb = stock.map((s,i)=>{
+          let promise = new Promise((resolve,reject) => {
+            db.collection('stocks').find({stock:s}).toArray((err,result)=>{
+              if (result.length > 0 ) {
+                likes[i] = result[0]["likes"]
+                resolve({modify: result[0]['like_ips'].includes(ip) == false & like===1})
+              } else {
+                resolve({modify: like===1})
+              }
+            })
+          })
+          .then((result)=>{
+            if (result.modify) {
+              console.log('Modify')
+              db.collection('stocks').findAndModify(
+                        {stock: s},
+                        {},
+                        { $push: {like_ips: ip},
+                          $inc: {likes: like} },
+                        {new:true, upsert: true},
+                        (err,result)=>{
+                          if (err) throw err
+                          likes[i] = result.value["likes"]
+                })
+            }
+          })
+          .catch((err) => { console.log(err) })
+          return promise
+        });
+
+        let promises = promisesApi.concat(promisesDb)
+
+        Promise.all(promises).then((values)=>{
+          // console.log('Resolved',stockData)
+          // console.log('likes',likes)
+          let rel_likes = likes[0]-likes[1]
+          console.log('rel_likes',rel_likes)
+          stockData[0].rel_likes = rel_likes
+          stockData[1].rel_likes = rel_likes > 0? -1*rel_likes: 0;
+          console.log('stockData',stockData)
+          res.json({stockData})
+        });
+      });
+    }
+  });
+}
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require('dotenv').load();
+
 var express     = require('express');
 var bodyParser  = require('body-parser');
 var expect      = require('chai').expect;
@@ -9,6 +11,8 @@ var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
 
+var helmet = require('helmet')
+
 var app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -17,6 +21,7 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet())
 
 //Index page (static HTML)
 app.route('/')
@@ -27,9 +32,9 @@ app.route('/')
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
-apiRoutes(app);  
-    
+//Routing for API
+apiRoutes(app);
+
 //404 Not Found Middleware
 app.use(function(req, res, next) {
   res.status(404)
